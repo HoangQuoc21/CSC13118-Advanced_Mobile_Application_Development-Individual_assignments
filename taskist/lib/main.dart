c4c0b2fe0942bdf1034c4a76203f841a9b44ee43
Hoàng Quốc <hoangquoc2106@gmail.com>
2024-11-08 13:36:10 +0700

    Thay icon cho app

--- taskist/lib/main.dart
+++ taskist/lib/main.dart
@@ -14,6 +14,7 @@ void main() async {
 
   // Request NOTIFICATION permission
   await requestNotificationPermission();
+  await requestExactAlarmPermission();
 
   // Initialize NotificationService
   final NotificationService notificationService = NotificationService();
